1b79f6478ee4356322f60df616d54ae35c54f83c
fsoussand <73390978+fsoussand@users.noreply.github.com>
2023-05-04 09:12:13 +0200

    Link to my spotify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { trackUrls } from './assets/track-urls';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTracks } from './lib/fetchTracks';
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  };
+
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+
+  if (tracks) console.log(tracks[0]?.track.name);
+
+  const firstTrackNameDiv = tracks ? (
+    <p>{tracks[0]?.track.name}</p>
+  ) : (
+    <p>'No title loaded'</p>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +35,10 @@ const App = () => {
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
       <div className="App-buttons"></div>
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+      <button onClick={goToNextTrack}>Next track</button>
+      <p>{tracks?.length}</p>
+      {firstTrackNameDiv}
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
+import { PlaylistTrack, SavedTrack, Track } from 'spotify-types';
+import { Playlist } from 'spotify-types/typings/playlist';
+
 const apiToken: string = '';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+export const fetchTracks = async (): Promise<SavedTrack[]> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack[] };
+
+  return data.items;
 };
